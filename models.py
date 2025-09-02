8f7a989f0ead0f8b81c267d31059826b6c4283e9
Vlad Stan <stan.v.vlad@gmail.com>
2025-09-02 11:56:31 +0300

    chore: must handle empty

--- models.py
+++ models.py
@@ -15,11 +15,11 @@ class OwnerData(BaseModel):
     <% endfor%>
     << cancel_comment >>"""
 
-class CreateClientData(BaseModel):
-    """<< cancel_comment >>
-    <% for field in client_table.editable_fields %><< field >>
-    <% endfor%>
-    << cancel_comment >>"""
+# class CreateClientData(BaseModel):
+#     """<< cancel_comment >>
+#     <% for field in client_table.editable_fields %><< field >>
+#     <% endfor%>
+#     << cancel_comment >>"""
 
 
 class ClientData(BaseModel):
