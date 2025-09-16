44d52434efe62a4255e0929a5885cf73680e3b9c
Vlad Stan <stan.v.vlad@gmail.com>
2025-09-16 14:55:50 +0300

    feat: add models

--- transform/models.py
+++ transform/models.py
@@ -1,3 +1,6 @@
+from lnbits.db import dict_to_model
+from pydantic import BaseModel
+
 extra_ui_fields = [
     {
         "name": "updated_at",
@@ -20,6 +23,63 @@ extra_ui_fields = [
         "sortable": True,
     },
 ]
+
+
+class DataField(BaseModel):
+    name: str
+    type: str
+    label: str | None = None
+    hint: str | None = None
+    optional: bool = False
+    editable: bool = False
+    searchable: bool = False
+    sortable: bool = False
+    fields: list["DataField"] = []
+
+
+class DataFields(BaseModel):
+    name: str
+    fields: list[DataField] = []
+
+
+class SettingsFields(DataFields):
+    enabled: bool = False
+    type: str = "user"  # "user" or "admin"
+
+
+class ActionFields(BaseModel):
+    generate_action: bool = False
+    wallet_id: str | None = None
+    currency: str | None = None
+    amount: str | None = None
+
+
+class OwnerDataFields(BaseModel):
+    name: str | None = None
+    description: str | None = None
+
+
+class ClientDataFields(BaseModel):
+    public_inputs: list[str] = []
+
+
+class PublicPageFields(BaseModel):
+    has_public_page: bool = False
+    owner_data_fields: OwnerDataFields
+    client_data_fields: ClientDataFields | None = None
+    action_fields: ActionFields
+
+
+class ExtensionData(BaseModel):
+    id: str
+    name: str
+    short_description: str | None = None
+    description: str | None = None
+    owner_data: DataFields
+    client_data: DataFields
+    settings_data: SettingsFields
+    public_page: PublicPageFields
+
 
 data2 = {
     "id": "donations",
@@ -135,9 +195,9 @@ data2 = {
         ],
     },
     "settings_data": {
+        "name": "Settings",
         "enabled": True,
         "type": "user",
-        "name": "Settings",
         "fields": [
             {
                 "name": "max_campaign_amount_sats",
@@ -194,6 +254,7 @@ data = {
         },
     },
     "settings_data": {
+        "name": "Settings",
         "enabled": True,
         "type": "user",
         "fields": [
@@ -311,3 +372,10 @@ data = {
     },
     "settingsType": "user",
 }
+
+
+# print("### data:", json.dumps(data))
+extension_data = dict_to_model(data, ExtensionData)
+
+# print("### extension_data:", extension_data)
+# print("### extension_data.json():", extension_data.json())
