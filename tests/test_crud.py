b83ef01a011863a1ea48135b39f2c9cc75656287
Vlad Stan <stan.v.vlad@gmail.com>
2026-01-15 14:17:26 +0200

    test: update owner data

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -6,11 +6,14 @@ from extension_builder_stub.crud import (  # type: ignore[import]
     create_owner_data,
     delete_owner_data,
     get_owner_data,
+    get_owner_data_by_id,
     get_owner_data_ids_by_user,
     get_owner_data_paginated,
+    update_owner_data,
 )
 from extension_builder_stub.models import (  # type: ignore[import]
     CreateOwnerData,
+    OwnerData,
 )
 
 
@@ -21,8 +24,7 @@ async def test_create_and_get_owner_data():
     data = CreateOwnerData(
         """<< cancel_comment >>
         <% for field in owner_data.random_fields_values %><< field >>
-        <% endfor%>
-        << cancel_comment >>"""
+        <% endfor%> << cancel_comment >>"""
     )
     owner_data_one = await create_owner_data(user_id, data)
     assert owner_data_one.id is not None
@@ -33,14 +35,12 @@ async def test_create_and_get_owner_data():
     assert owner_data_one.user_id == user_id
     """<< cancel_comment >>
     <% for field in owner_data.fields %>assert owner_data_one.<< field >> == data.<< field >>
-    <% endfor%>
-    << cancel_comment >>"""
+    <% endfor%>  << cancel_comment >>"""
 
     data = CreateOwnerData(
         """<< cancel_comment >>
         <% for field in owner_data.random_fields_values %><< field >>
-        <% endfor%>
-        << cancel_comment >>"""
+        <% endfor%> << cancel_comment >>"""
     )
     owner_data_two = await create_owner_data(user_id, data)
     assert owner_data_two.id is not None
@@ -60,3 +60,37 @@ async def test_create_and_get_owner_data():
     owner_data_page = await get_owner_data_paginated(user_id=user_id)
     assert owner_data_page.total == 1
     assert len(owner_data_page.data) == 1
+
+
+@pytest.mark.asyncio
+async def test_update_owner_data():
+    user_id = uuid4().hex
+
+    data = CreateOwnerData(
+        """<< cancel_comment >>
+        <% for field in owner_data.random_fields_values %><< field >>
+        <% endfor%>  << cancel_comment >>"""
+    )
+    owner_data_one = await create_owner_data(user_id, data)
+    assert owner_data_one.id is not None
+    assert owner_data_one.user_id == user_id
+
+    owner_data_one = await get_owner_data(user_id, owner_data_one.id)
+    assert owner_data_one.id is not None
+    assert owner_data_one.user_id == user_id
+    """<< cancel_comment >>
+    <% for field in owner_data.fields %>assert owner_data_one.<< field >> == data.<< field >>
+    <% endfor%>  << cancel_comment >>"""
+
+    data_updated = CreateOwnerData(
+        """<< cancel_comment >>
+        <% for field in owner_data.random_fields_values %><< field >>
+        <% endfor%>  << cancel_comment >>"""
+    )
+    owner_data_updated = OwnerData(**{**owner_data_one.dict(), **data_updated.dict()})
+
+    await update_owner_data(owner_data_updated)
+    owner_data_one = await get_owner_data_by_id(owner_data_one.id)
+    """<< cancel_comment >>
+    <% for field in owner_data.fields %>assert owner_data_one.<< field >> == owner_data_updated.<< field >>
+    <% endfor%>  << cancel_comment >>"""
